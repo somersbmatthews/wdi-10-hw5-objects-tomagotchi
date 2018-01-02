73f643fb62516303a88eedfb3b0663f7116f41ef
Somers Matthews <somersbmatthews@uchicago.edu>
2018-01-02 00:54:39 -0600

    Commit 17 - updated puke and yawn methods on both

--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -10,10 +10,14 @@ var tamagotchi1 = {
     console.log("There is " + this.food + " in "+this.name+"'s tummy.");
   },
   puke: function(){
+    this.health--;
     console.log(this.name + ": Bleeeeh :O~~");
+    console.log(this.name +" has " +this.health+ " left.");
   },
   yawn: function(){
+    this.stamina--;
     console.log(this.name + ": Yaaaawwwwn!");
+    console.log(this.name +" has " +this.stamina+ " left.");
   }
 }
 
@@ -27,6 +31,12 @@ var tamagotchi1 = {
 // 7. Update the `yawn` and `puke` methods to output the Tamogatchi's name in the sentence with the yawn or puke action. Execute these methods to confirm the output.
 // 8. Update the `cry` method to decrease the amount of food in the Tamagotchi's tummy.
 // 9. In addition, the `cry` method should now also log a sentence that will tell us the amount of food in the Tamagotchi's tummy.
+
+// Make it so that when puke is invoked, it decreases the object's health value by 1 and logs the new number to the console, and also logs the Tamagotchi's name
+//
+// Make it so that when yawn is invoked, it decreases the object's restedness value by 1 and logs the new number to the console, and also logs the Tamagotchi's name
+
+
 var tamagotchi2 = {
   name: "Peter",
   creatureType: "Dragon",
@@ -39,9 +49,20 @@ var tamagotchi2 = {
     console.log("There is " + this.food + " in "+this.name+"'s tummy.");
   },
   puke: function(){
+    this.health--;
     console.log(this.name + ": Bleeeeh :O~~");
+    console.log(this.name +" has " +this.health+ " left.");
   },
   yawn: function(){
+    this.stamina--;
     console.log(this.name + ": Yaaaawwwwn!");
+    console.log(this.name +" has " +this.stamina+ " left.");
+  }
+}
+
+let player = {
+  name: "Somers",
+  sayName: function(){
+
   }
 }
