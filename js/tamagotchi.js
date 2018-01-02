dd54323d8ab548e47c70ed65be1b791274109281
Somers Matthews <somersbmatthews@uchicago.edu>
2018-01-02 00:16:41 -0600

    Commit 14 - Made a yawn method

--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -9,9 +9,16 @@ var tamagotchi1 = {
   },
   puke: function(){
     console.log("Bleeeeh :O~~");
+  },
+  yawn: function(){
+    console.log("Yaaaawwwwn!");
   }
 }
 
 // 2. Let's add a method of `cry` to our Tamagotchi that will log "Waaaaaa!!!" anytime the `cry` method is called.
 //
 // 3. Invoke the `cry` method to test that it works.
+
+// 5. Write a method `yawn` into your Tamagotchi object that console.logs the Tamagotchi yawning when it is tired (example: `Yaaaawwwwn!`).
+//
+// 6. Invoke the `yawn` method.
